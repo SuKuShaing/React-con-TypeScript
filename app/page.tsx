2225d52a685fad42658c1bce97a3444c39af2814
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-08-13 21:05:47 -0400

    añadiendo tipos a un hook de React

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,40 @@
+"use client";
+/**
+ * Desde la versión 13+ Next.js por defecto usa server side rendering,
+ * es decir, el servidor renderiza la página y luego el cliente la renderiza,
+ * dado que useState es un hook que solo se puede usar en el cliente,
+ * debemos usar "use client" para que el servidor no renderice la página,
+ * sino que el cliente la renderice.
+ */
+
 import { RandomFox } from "@/components/RandomFox";
-import Image from "next/image";
+import { useState } from "react";
+
+const random = (): number => {
+	return Math.floor(Math.random() * 124) + 1;
+};
+const urlFox = `https://randomfox.ca/images/${random()}.jpg`;
 
 export default function Home() {
-
-	const random = (): number => {
-		return Math.floor(Math.random() * 124) + 1
-	};
-
-	const urlFox = `https://randomfox.ca/images/${random()}.jpg`;
+	const [images, setImages] = useState<string[]>([
+		`https://randomfox.ca/images/${random()}.jpg`,
+		`https://randomfox.ca/images/${random()}.jpg`,
+		`https://randomfox.ca/images/${random()}.jpg`,
+		`https://randomfox.ca/images/${random()}.jpg`,
+		`https://randomfox.ca/images/${random()}.jpg`,
+		`https://randomfox.ca/images/${random()}.jpg`,
+		`https://randomfox.ca/images/${random()}.jpg`,
+	]);
 
 	return (
-		// <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-		// 	<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-		// 		<h1>Hello, Platzi</h1>
-		// 	</main>
-		// 	<footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
-		// </div>
 		<>
 			<main className="flex flex-col items-center">
 				<h1 className="text-4xl font-bold">Zorritos Platzi</h1>
-				<RandomFox image={urlFox} alt="Un lindo zorrito" />
+				{images.map((image, index) => (
+					<div key={index} className="p-2">
+						<RandomFox image={image} alt="Un lindo zorrito" />
+					</div>
+				))}
 			</main>
 			<footer></footer>
 		</>
