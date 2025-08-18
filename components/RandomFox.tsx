78ecc537dd44d6fc3366a35ea75be5ea173a25a0
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-08-18 17:55:39 -0400

    se tipa useRef

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,5 +1,6 @@
-import { FC, FunctionComponent, JSX } from "react";
+import { FC, FunctionComponent, JSX, useRef } from "react";
 
+/*
 export const RandomFox1 = () => {                       // implícito
 	return <img />;
 };
@@ -15,15 +16,16 @@ export const RandomFox3: FunctionComponent = () => {    // explícito y tipamos 
 export const RandomFox4: FC = () => {                   // explícito y tipamos la constante con una abreviatura
 	return <img />;
 };
-
-
-
+*/
 
 type Props = { image: string; alt: string };
 
 export const RandomFox = ({ image, alt }: Props): JSX.Element => {
+	const node = useRef<HTMLImageElement>(null); // HTMLImageElement, se le pasa el tipo de donde ser usará
+
 	return (
 		<img
+			ref={node}
 			src={image}
 			width={512}
 			height="auto"
